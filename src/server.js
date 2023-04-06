e5be3a7f426b09e67cc816ca2b4231adfb103886
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-06 16:33:28 -0300

    buffers

--- src/server.js
+++ src/server.js
@@ -2,25 +2,39 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
-    return response
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
-    return response.writeHead(201).end();
+    return res.writeHead(201).end();
   }
 
-  return response.writeHead(404).end("Not found");
+  return res.writeHead(404).end("Not found");
 });
 
 server.listen(3333);
