b7c8865f12a496194eee95ae6d908c80475bc0f3
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-06 15:46:12 -0300

    using a complete stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -22,7 +22,7 @@ class InverseNumberStream extends Transform {
   _transform(chunk, encoding, callback) {
     const transformed = Number(chunk.toString()) * -1
 
-    callback(null, Buffer.from(String(transformed)))
+    callback(null, Buffer.from((transformed)))
   }
 }
 
