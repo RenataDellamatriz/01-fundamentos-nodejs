61defc01f7a14b143d360679a04b591296a94a55
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-13 11:33:50 -0300

    feat: regex for parameters

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,9 @@
+///users/:id
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-Z0-9\-_]+)')
+
+ const pathRegex = new RegExp(`^${pathWithParams}`)
+
+ return pathRegex
+}
