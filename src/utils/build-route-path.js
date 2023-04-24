fc764a043026452d1b8a6424b2d8586781d3c91a
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-24 14:09:39 -0300

    feat: filtering users

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,9 +1,12 @@
 ///users/:id
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-Z0-9\-_]+)')
+  const routeParametersRegex = /:([a-zA-z]+)/g;
+  const pathWithParams = path.replaceAll(
+    routeParametersRegex,
+    "(?<$1>[a-zA-Z0-9-_]+)"
+  );
 
- const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
- return pathRegex
+  return pathRegex;
 }
