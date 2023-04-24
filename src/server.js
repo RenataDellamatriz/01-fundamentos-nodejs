6dfda5c7ee50c3a71f1b1677ee44519aae9c763c
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-23 23:31:24 -0300

    feat: deleting user

--- src/server.js
+++ src/server.js
@@ -2,20 +2,20 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
 
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  const route = routes.find(route => {
-    return route.method === method && route.path.test(url)
-  })
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
   if (route) {
-    const routeParams = req.url.match(route.path)
-    console.log(routeParams)
-    return route.handler(req,res)
+    const routeParams = req.url.match(route.path);
+    req.params = { ...routeParams.groups };
+
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end("Not found");
